7d6d8403d935e9af94f3e74341068ef32c3c55a9
Eko <greget103@gmail.com>
2022-01-27 14:45:00 +0700

    Add Travelio for scrapping

---
+++ Hotel_travelio/main.py
@@ -0,0 +1,7 @@
+from scrapping import get_data
+
+#use this code to inform this main file
+
+if __name__ == '__main__':
+    print('This main file')
+    hasil = get_data()
--- Hotel_travelio/scrapping/__init__.py
+++ Hotel_travelio/scrapping/__init__.py
@@ -8,18 +8,26 @@ from bs4 import BeautifulSoup
 city = 'Depok'  #ex 'cibinong'
 check_in = 'checkIn=26-01-2022&'   #dd-mm-yyyy
 check_out = 'checkOut=26-02-2022&'  #dd-mm-yyyy
-url = f"https://www.travelio.com/search?searchType=monthly&destinationCategory=City&destinationUrlName=&" \
-      f"destinationPlaceId=&destinationCountryId=ID&destinationId=546c9db1b094a31e06e08a6b&nights=31&flexible=1&" \
-      f"destination={city}&{check_in}{check_out}months=1&cbFlexible=on&unitType=3%2C2%2C1%2Cstudio&" \
-      f"propTypeId=guesthouse%2Chostel%2Chotel%2Croom%2Cvilla%2Chouse%2Capartment&" \
-      f"sellType=Unfurnished%2CFull%2BFurnished&bottomPrice=833333.6000000001&upperPrice=15523333.6"
-headers = {
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
-}
 
 # Get data from link
-tampil = requests.get(url)
-print(tampil.status_code)
+def get_data():
+    url = f"https://www.travelio.com/search?searchType=monthly&destinationCategory=City&destinationUrlName=&" \
+          f"destinationPlaceId=&destinationCountryId=ID&destinationId=546c9db1b094a31e06e08a6b&nights=31&flexible=1&" \
+          f"destination={city}&{check_in}{check_out}months=1&cbFlexible=on&unitType=3%2C2%2C1%2Cstudio&" \
+          f"propTypeId=guesthouse%2Chostel%2Chotel%2Croom%2Cvilla%2Chouse%2Capartment&" \
+          f"sellType=Unfurnished%2CFull%2BFurnished&bottomPrice=833333.6000000001&upperPrice=15523333.6"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/97.0.4692.99 Safari/537.36"
+    }
+    
+    try:
+        source = requests.get(url,  headers= headers)
+    except Exception:
+        return None
+    if source.status_code == 200:
+        print(source.status_code)
+        soup = BeautifulSoup(source.text,   'html.parser')
+        #data = soup.find('div', id = 'hotel-list-box-v2')
+        print(soup.prettify())
 
-soup = BeautifulSoup(tampil.text,   'html.parser')
-print(soup.prettify())
